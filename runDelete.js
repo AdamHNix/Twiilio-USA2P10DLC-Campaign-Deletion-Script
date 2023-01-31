13110fbddae69a3a9b3a2bf4dd7cef4d18f3f386
Adam Nix <adam.h.nix@gmail.com>
2023-01-30 19:54:23 -0500

    add error reporting

--- runDelete.js
+++ runDelete.js
@@ -1,24 +1,27 @@
 require('dotenv').config()
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
+const apiKey = process.env.TWILIO_API_KEY;
+const apiSecret = process.env.TWILIO_API_SECRET;
 const fs = require('fs');
-const client = require('twilio')(accountSid, authToken);
+const client = require('twilio')(apiKey, apiSecret, { accountSid: accountSid });
 //for csv parsing
 const csv = require('papaparse');
 //add csv directory here
 const csvFile = fs.createReadStream("startersTest.csv");
 //List number of rows to be parsed
 const rows = 3
+let element = 0;
 
 //call needed to delete campaign on a given MG SID
 function deleteStarter(SID){
     client.messaging.v1.services(SID)
         .usAppToPerson('QE2c6890da8086d771620e9b13fadeba0b')
         .remove()
-}
+        .catch( e =>{
+            console.log (`error in delete for ${SID}: `, e)
+        })}
 
 async function runParse(file){
-    var element = 0;
     csv.parse(file, {
     delimiter: ',',
     preview: rows,
@@ -26,13 +29,15 @@ async function runParse(file){
     complete: 
         function(results) {
             while(element < rows){
+                console.log("element ", element)
                 var MGSID = (results.data[element][0])
                 //run Twilio deletion
-                deleteStarter(MGSID);
+                try{deleteStarter(MGSID)}catch(e){
+                    console.log("Error: ", e)
+                }
+                console.log(`${MGSID} deleted`)
                 element++;
                 //delay function needed due to recommendation of only 10 calls per second
-                setTimeout(() => {
-                }, "100")
             }
         }
     })
